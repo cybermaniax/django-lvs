d1cad7abedd28146219e818c33a6ad1965cd8820
Grzegorz Halajko <gh@linuxpolska.pl>
2015-01-12 13:24:38 +0000

    simple adding virtual server

--- src/ipvsadmin/ipvsadm.py
+++ src/ipvsadmin/ipvsadm.py
@@ -37,3 +37,23 @@ def weight_real_server(mode,port,realserver,weight,realsmode):
     command.append(weight) 
     command.append(__RS_MODE[realsmode])    
     return call(command,stderr=subprocess.STDOUT)
+
+def add_virtual_server(ip,port,fwmark,mode,peristtimeout,scheduler):
+    command = [settings.IPVSADMIN,]
+    command.append('-A') 
+    command.append(__LVS_MODE[mode])
+    if 'TCP' in mode or 'UDP' in mode:
+        command.append("%s:%s"%(ip,port))
+    elif 'FWM' in mode:
+        command.append(fwmark)
+    else:
+        raise ValueError('No support %s'%fwmark)
+    
+    if peristtimeout:
+        command.append('-p')
+        command.append(peristtimeout)
+        
+    command.append('-s')
+    command.append(scheduler)
+    
+    return call(command,stderr=subprocess.STDOUT)
